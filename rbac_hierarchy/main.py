80efd366476b00f7462c1f8dc65d4afcd98060a2
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-12-24 22:51:53 +0100

    refactored

--- rbac_hierarchy/main.py
+++
@@ -1,9 +0,0 @@
-from src.database import get_db
-
-
-def main():
-    db = get_db()
-
-
-if __name__ == "__main__":
-    main()
--- real_time_analyze/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from real-time-analyze!")
-
-
-if __name__ == "__main__":
-    main()
